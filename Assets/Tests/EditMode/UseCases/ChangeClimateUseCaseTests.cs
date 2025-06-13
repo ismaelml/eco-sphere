58b3bbb575f84729cd5d03b4a3a7e751cf3fd952
Ismael <ismaelj0793@gmail.com>
2025-06-13 13:07:02 -0400

    test: adding test for ChangeClimateUseCase

--- Assets/Tests/EditMode/UseCases/ChangeClimateUseCaseTests.cs
+++ Assets/Tests/EditMode/UseCases/ChangeClimateUseCaseTests.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using NUnit.Framework;
+using EcoSphere.Domain;
+using EcoSphere.UseCases;
 
-public class ChangeClimateUseCaseTests : MonoBehaviour
+namespace EcoSphere.Tests.UseCases
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ChangeClimateUseCaseTests
     {
-        
-    }
+        [Test]
+        public void Execute_ShouldChangeWeather()
+        {
+            var climate = new Climate(WeatherType.Sunny);
+            var useCase = new ChangeClimateUseCase(climate);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            useCase.Execute(WeatherType.Rainy);
+
+            Assert.AreEqual(WeatherType.Rainy, climate.CurrentWeather);
+        }
+
+        [Test]
+        public void Execute_ShouldAllowMultipleWeatherChanges()
+        {
+            var climate = new Climate(WeatherType.Cloudy);
+            var useCase = new ChangeClimateUseCase(climate);
+
+            useCase.Execute(WeatherType.Rainy);
+            Assert.AreEqual(WeatherType.Rainy, climate.CurrentWeather);
+
+            useCase.Execute(WeatherType.Sunny);
+            Assert.AreEqual(WeatherType.Sunny, climate.CurrentWeather);
+        }
     }
 }
